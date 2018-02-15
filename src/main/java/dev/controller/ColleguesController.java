8479c23c415b2bde18385f39a9cf22c76e718302
quentinboudet <quentin.boudet@outlook.fr>
2018-02-15 18:13:33 +0100

    @CrossOrigin

--- src/main/java/dev/controller/ColleguesController.java
+++ src/main/java/dev/controller/ColleguesController.java
@@ -7,13 +7,13 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dev.entite.Collegue;
@@ -21,6 +21,7 @@ import dev.repository.ColleguesRepository;
 
 @Component
 @RequestMapping("/collegues")
+@CrossOrigin
 public class ColleguesController {
 	
 	@Autowired
